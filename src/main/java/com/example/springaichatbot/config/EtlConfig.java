239cd1c2c91414d52472910e21f53b6eaea23545
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2025-05-11 09:09:48 +0700

    update

--- src/main/java/com/example/springaichatbot/config/EtlConfig.java
+++ src/main/java/com/example/springaichatbot/config/EtlConfig.java
@@ -54,7 +54,7 @@ public class EtlConfig {
                         log.info("Processing file: {}", filePath);
                         var tikaDocumentReader = new TikaDocumentReader(new FileSystemResource(message.getPayload().toString()));
                         var documents = tikaDocumentReader.read();
-                        var splitter = new TokenTextSplitter(512, 100, 20, 1000, true);
+                        var splitter = new TokenTextSplitter(800, 200, 30, 3000, true);
                         documents = splitter.apply(documents);
                         chromaVectorStore.accept(documents);
                         log.info("Processed file: {}", filePath);
