60a4cbf3a88ad538630effe94e0d0077b856aa25
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2025-06-04 13:08:38 +0700

    update: DateTimeTools

--- src/main/java/com/example/springaichatbot/tool/DateTimeTools.java
+++ src/main/java/com/example/springaichatbot/tool/DateTimeTools.java
@@ -4,15 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 @Slf4j
 @Component
 public class DateTimeTools {
     @Tool(description = "Get the current date and time in the user's timezone")
     String getCurrentDateTime() {
-        String now = ZonedDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh")).toLocalDateTime().atZone(ZoneId.of("Asia/Ho_Chi_Minh")).toString();
+        String now = LocalDateTime.now(ZoneId.of("Asia/Ho_Chi_Minh")).atZone(ZoneId.of("Asia/Ho_Chi_Minh")).toString();
         log.info("Current date and time in Asia/Ho_Chi_Minh timezone: {}", now);
         return now;
     }
