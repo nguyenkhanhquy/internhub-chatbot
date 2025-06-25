7eb838ee7b5bfd92a18b71e53bb6be683b80cb63
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2025-06-25 18:22:36 +0700

    update: AiConfig

--- src/main/java/com/example/springaichatbot/config/AiConfig.java
+++ src/main/java/com/example/springaichatbot/config/AiConfig.java
@@ -59,6 +59,9 @@ public class AiConfig {
     @Value("${spring.ai.openai.chat.base-url}")
     private String openaiBaseUrl;
 
+    @Value("${spring.ai.openai.chat.completions-path}")
+    private String completionsPath;
+
     @Value("${spring.ai.openai.chat.options.model}")
     private String openaiModel;
 
@@ -67,7 +70,11 @@ public class AiConfig {
 
     @Bean
     public OpenAiApi openAiApi() {
-        return OpenAiApi.builder().apiKey(openaiApiKey).baseUrl(openaiBaseUrl).build();
+        return OpenAiApi.builder()
+                .apiKey(openaiApiKey)
+                .baseUrl(openaiBaseUrl)
+                .completionsPath(completionsPath)
+                .build();
     }
 
     @Bean
